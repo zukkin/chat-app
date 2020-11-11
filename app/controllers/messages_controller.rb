4d5ad4681aaae07c94fd642dd9c66864bb51b9c1
zukkin <73934559+zukkin@users.noreply.github.com>
2020-11-11 23:12:25 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+       redirect_to room_message_path(@room)
+    else
+       render :index
+    end
+  end
+  
+    private
+  
+    def message_params
+      params.require(:message).permit(:content).merge(user_id: current_user.id)
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
