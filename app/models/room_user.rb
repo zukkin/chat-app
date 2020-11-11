9479568f3e745dbbf05b5a39250036110943fa6a
zukkin <73934559+zukkin@users.noreply.github.com>
2020-11-11 21:13:53 +0900

    messagesテーブルの作成

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,7 @@
 class RoomUser < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
+  has_many :room_users
+  has_many :users, through: :room_users
+  has_many :messages
+
+  validates :name, presence: true
 end
