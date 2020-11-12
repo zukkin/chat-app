c730fd070ee74f8f9dd29379fc21fd470fd71f96
zukkin <73934559+zukkin@users.noreply.github.com>
2020-11-12 11:11:13 +0900

    投稿時刻の日本時間への変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.lord_default_locale = :javascripts
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
