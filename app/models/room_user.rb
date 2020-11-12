a4c465fd36842dceebf14b6ec09d3a003568ef0f
zukkin <73934559+zukkin@users.noreply.github.com>
2020-11-12 11:43:21 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,7 @@
 class RoomUser < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  # has_many :room_users
+  belongs_to :room
+  belongs_to :user
 
   validates :name, presence: true
 end
